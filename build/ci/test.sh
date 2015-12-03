fe2a94c83a2b8c5a3f5619ecd5509d1a5b182db5
timonwong <wangtianpeng@navercorp.com>
2015-12-03 10:12:05 +0800

    Only upload coverage when specified explicitly

--- build/ci/test.sh
+++ build/ci/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "${TRAVIS_PULL_REQUEST}" == 'false' || -z "${CI_PULL_REQUEST}"]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == 'false' || -z "${CI_PULL_REQUEST}" ]]; then
     npm run build
     npm run lint
     npm run cover
-    cat ./coverage/lcov.info | ./node_modules/.bin/codecov
+    if [[ "${CODECOV_UPLOAD}" == "true" ]]; then
+        echo "Uploading coverage reports to codecov.io ..."
+        ./node_modules/.bin/codecov < ./coverage/lcov.info
+    fi
 else
     npm test
 fi
