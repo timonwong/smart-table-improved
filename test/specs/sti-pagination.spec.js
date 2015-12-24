e5681b312325fa6217a3b45daeceba145e103ac2
timonwong <wangtianpeng@navercorp.com>
2015-12-24 14:41:08 +0800

    Add more tests for pagination

--- test/specs/sti-pagination.spec.js
+++ test/specs/sti-pagination.spec.js
@@ -1,6 +1,7 @@
 describe('sti-pagination directive', () => {
   let $compile;
   let $rootScope;
+  let $scope;
   let stiPaginationConfig;
   let element;
   let tableState;
@@ -22,6 +23,23 @@ describe('sti-pagination directive', () => {
     }
   };
 
+  function compileElement(paginationState, {itemsPerPage = undefined, templateUrl = undefined, onPageChange = undefined} = {}) {
+    let attrs = [];
+    if (angular.isDefined(itemsPerPage)) {
+      attrs.push(`items-per-page="${itemsPerPage}"`);
+    }
+    if (angular.isDefined(templateUrl)) {
+      attrs.push(`template-url="${templateUrl}"`);
+    }
+    if (angular.isDefined(onPageChange)) {
+      attrs.push(`on-page-change="${onPageChange}"`);
+    }
+    let template = `<div st-table="rowCollection"><table></table><sti-pagination ${attrs.join(' ')}></sti-pagination></div>`;
+    element = $compile(template)($scope);
+    $.extend(tableState.pagination, paginationState);
+    $scope.$apply();
+  }
+
   function getPageLinksArray() {
     return element.find('ul.pagination li').map(function() {
       return $(this).text().trim();
@@ -42,7 +60,8 @@ describe('sti-pagination directive', () => {
     $rootScope = _$rootScope_;
     stiPaginationConfig = _stiPaginationConfig_;
 
-    $rootScope.rowCollection = rowCollection;
+    $scope = $rootScope.$new();
+    $scope.rowCollection = rowCollection;
 
     // Reset tableState
     tableState = {
@@ -55,17 +74,15 @@ describe('sti-pagination directive', () => {
   describe('init', () => {
     it('should call for the first page with default itemsPerPage if "items-per-page" is unset', () => {
       spyOn(controllerMock, 'slice');
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination></sti-pagination></div>`;
-      $compile(template)($rootScope);
-      $rootScope.$apply();
+      compileElement();
+
       expect(controllerMock.slice).toHaveBeenCalledWith(0, stiPaginationConfig.itemsPerPage);
     });
 
     it('should call for the first page on init', () => {
       spyOn(controllerMock, 'slice');
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination items-per-page="5"></sti-pagination></div>`;
-      $compile(template)($rootScope);
-      $rootScope.$apply();
+      compileElement({}, {itemsPerPage: 5});
+
       expect(controllerMock.slice).toHaveBeenCalledWith(0, 5);
     });
 
@@ -73,9 +90,8 @@ describe('sti-pagination directive', () => {
       let defaultItemsPerPage = stiPaginationConfig.itemsPerPage;
       stiPaginationConfig.itemsPerPage = 23;
       spyOn(controllerMock, 'slice');
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination></sti-pagination></div>`;
-      $compile(template)($rootScope);
-      $rootScope.$apply();
+      compileElement();
+
       expect(controllerMock.slice).toHaveBeenCalledWith(0, 23);
       stiPaginationConfig.itemsPerPage = defaultItemsPerPage;
     });
@@ -91,50 +107,28 @@ describe('sti-pagination directive', () => {
     it('should load custom template from stiPaginationConfig\'s "template-url"', () => {
       let defaultTemplate = stiPaginationConfig.templateUrl;
       stiPaginationConfig.templateUrl = 'custom_stiPaginationConfig_templateUrl.html';
-
       $templateCache.put('custom_stiPaginationConfig_templateUrl.html', `<ul id="custom_stiPaginationConfig_templateUrl"></ul>`);
+      compileElement();
 
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination></sti-pagination></div>`;
-      element = $compile(template)($rootScope);
-      $rootScope.$apply();
       expect(element.find('#custom_stiPaginationConfig_templateUrl').length).toBe(1);
       stiPaginationConfig.templateUrl = defaultTemplate;
     });
 
     it('should load custom template from attribute "template-url"', () => {
       $templateCache.put('custom_templateUrl.html', `<ul id="custom_templateUrl"></ul>`);
+      compileElement({}, {templateUrl: 'custom_templateUrl.html'});
 
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination template-url="custom_templateUrl.html"></sti-pagination></div>`;
-      element = $compile(template)($rootScope);
-      $rootScope.$apply();
       expect(element.find('#custom_templateUrl').length).toBe(1);
     });
   });
 
   describe('draw pages', () => {
-    beforeEach(() => {
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination></sti-pagination></div>`;
-      element = $compile(template)($rootScope);
-    });
-
-    function compileElement(paginationState, {itemsPerPage = undefined} = {}) {
-      let attrs = [];
-      if (angular.isDefined(itemsPerPage)) {
-        attrs.push(`items-per-page="${itemsPerPage}"`);
-      }
-      let template = `<div st-table="rowCollection"><table></table><sti-pagination ${attrs.join(' ')}></sti-pagination></div>`;
-      element = $compile(template)($rootScope);
-      $.extend(tableState.pagination, paginationState);
-      $rootScope.$apply();
-    }
-
     it('should show the correct pagination links at start of sequence', () => {
       compileElement({
         start: 0,
         numberOfPages: Math.ceil(rowCollection.length / 10),
         number: 10
       });
-      $rootScope.$apply();
 
       let pageLinks = getPageLinksArray();
 
@@ -148,7 +142,6 @@ describe('sti-pagination directive', () => {
         numberOfPages: Math.ceil(rowCollection.length / 10),
         number: 10
       });
-      $rootScope.$apply();
 
       let pageLinks = getPageLinksArray();
 
@@ -167,6 +160,51 @@ describe('sti-pagination directive', () => {
 
       expect(pageLinks.length).toBe(12);
       expect(pageLinks).toEqual(['‹', '1', '...', '13', '14', '15', '16', '17', '18', '19', '20', '›']);
+    });
+  });
+
+  describe('on-page-change callback', () => {
+    beforeEach(() => {
+      $scope.onPageChange = jasmine.createSpy('onPageChange');
+    });
+
+    it('should call the callback once when page link clicked', function() {
+      compileElement({
+        number: 10,
+        numberOfPages: Math.ceil(rowCollection.length / 10)
+      }, {
+        itemsPerPage: 13, onPageChange: 'onPageChange(newPage)'
+      });
+
+      let pagination = element.find('ul.pagination');
+
+      expect($scope.onPageChange).toHaveBeenCalledWith(1);
+      expect($scope.onPageChange.calls.count()).toEqual(1);
+
+      pagination.children().eq(2).find('a').triggerHandler('click');
+      $scope.$apply();
+
+      expect($scope.onPageChange).toHaveBeenCalledWith(2);
+      expect($scope.onPageChange.calls.count()).toEqual(2);
+    });
+
+    it('should not call when current page is not changed', () => {
+      compileElement({
+        number: 10,
+        numberOfPages: Math.ceil(rowCollection.length / 10)
+      }, {
+        itemsPerPage: 13, onPageChange: 'onPageChange(newPage)'
+      });
+
+      let pagination = element.find('ul.pagination');
+
+      expect($scope.onPageChange).toHaveBeenCalledWith(1);
+      expect($scope.onPageChange.calls.count()).toEqual(1);
+
+      pagination.children().eq(1).find('a').triggerHandler('click');
+      $scope.$apply();
+
+      expect($scope.onPageChange.calls.count()).toEqual(1);
     });
   });
 });
