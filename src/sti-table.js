7862062635fff5c8659deabfbe6d92f4dda42047
timonwong <wangtianpeng@navercorp.com>
2015-12-25 10:47:00 +0800

    Add `onPagination` callback to stiTable directive
    Since it's weird to put `currentPage` and `totalItemCount` two-way
    binding inside `stiPagination` directive, now they are moved to a
    callback inside `stiTable` directive, which may look better than
    previous implementation.

--- src/sti-table.js
+++ src/sti-table.js
@@ -5,6 +5,23 @@ angular
   .directive('stiTable', stiTable);
 
 
+/**
+ * @ngdoc directive
+ * @name smart-table-improved.directive:stiTable
+ * @restrict A
+ * @scope true
+ *
+ * @description
+ * The stiTable directive is a helper to stTable directive.
+ *
+ * @element table st-table='rowCollection'
+ * @param {string} defaultSort
+ * @param {string} defaultSortReverse
+ * @param {Expression} onPagination Expression to evaluate upon pagination state
+ * changes. (Pagination object is available as $pagination, with `currentPage`,
+ * `numberOfPages` and `totalItemCount` inside)
+ *
+ */
 stiTable.$inject = ['$parse'];
 function stiTable($parse) {
   return {
@@ -16,10 +33,30 @@ function stiTable($parse) {
 
   function link(scope, element, attrs, ctrl) {
     let stTableCtrl = ctrl;
+    let onPaginationHandler;
+
+    if (attrs.onPagination) {
+      onPaginationHandler = $parse(attrs.onPagination);
+
+      scope.$watch(() => {
+        return stTableCtrl.tableState().pagination;
+      }, handlePaginationChange, true);
+    }
 
     if (attrs.defaultSort) {
       let reverse = !!$parse(attrs.defaultSortReverse)(scope);
       stTableCtrl.sortBy(attrs.defaultSort, reverse);
+    }
+
+    function handlePaginationChange() {
+      let paginationState = stTableCtrl.tableState().pagination;
+      let pagination = {
+        currentPage: Math.floor(paginationState.start / paginationState.number) + 1,
+        numberOfPages: paginationState.numberOfPages,
+        totalItemCount: paginationState.totalItemCount
+      };
+
+      onPaginationHandler(scope, {$pagination: pagination});
     }
   }
 }
